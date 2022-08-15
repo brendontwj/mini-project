b60f2ca807b495c40689152521f728835410239c
brendontwj <bren0019@e.ntu.edu.sg>
2022-08-15 17:43:32 +0800

    init

---
+++ miniproject/src/test/java/vttp2022/batch2b/miniproject/MiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.batch2b.miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
