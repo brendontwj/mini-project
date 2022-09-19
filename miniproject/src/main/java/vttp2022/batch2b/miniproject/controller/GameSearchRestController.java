3d781f42f1190ab67f5bd57430d631301fc87252
brendontwj <bren0019@e.ntu.edu.sg>
2022-09-19 16:41:01 +0800

    Completed

--- miniproject/src/main/java/vttp2022/batch2b/miniproject/controller/GameSearchRestController.java
+++ miniproject/src/main/java/vttp2022/batch2b/miniproject/controller/GameSearchRestController.java
@@ -7,9 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +31,14 @@ public class GameSearchRestController {
     @Autowired
     GameRedis redisService;
 
-    @Autowired
-    User restUser;
-
+    @GetMapping(path = "/search/{id}")
+    public ResponseEntity getArticleById(@PathVariable String id) {
+        logger.info("inside get user codeblock");
+        System.out.println("Path variable is: " + id);
+        User user = redisService.findById(id);
+        if(user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cannot find user: " + id);
+        }
+        return ResponseEntity.ok(user);
+    }
 }
